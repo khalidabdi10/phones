235a09f2ef373d2d90406282eaff66cf322e205a
khalidabdi10 <khalidabdirashid7000@gmail.com>
2025-10-29 21:28:42 +0300

    second commit

--- Phones.java
+++ Phones.java
@@ -5,20 +5,23 @@ class Phone {
     public Phone(String brand) {
         this.brand = brand;
     }
+
     public void displayBrand() {
         System.out.println("Brand: " + brand);
     }
+
     public final void ring() {
-        System.out.println("Phone is ringing...");
+        System.out.println("Phone is ringing....");
     }
 }
+
 class SmartPhone extends Phone {
     public SmartPhone(String brand) {
         super(brand);
     }
 
     public void showInfo() {
-        super.displayBrand(); 
+        super.displayBrand();
         System.out.println("This is a smartphone.");
     }
 }
@@ -26,7 +29,7 @@ class SmartPhone extends Phone {
 public class Phones {
     public static void main(String[] args) {
         SmartPhone sp = new SmartPhone("Apple");
-        sp.showInfo(); 
-        sp.ring();     
+        sp.showInfo();
+        sp.ring();
     }
 }
